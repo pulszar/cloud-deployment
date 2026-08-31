69b40520e8fd588c454ac0ee1a2c7b2d015eb8d8
Luke Mattson <lukemattson03@gmail.com>
2026-08-30 23:36:41 -0500

    Resource group and vnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,8 +5,22 @@ terraform {
             version = "~> 3.0.2"
         }
     }
+    required_version = "1.15.8"
 }
 
 provider "azurerm" {
-    feature {}
+    features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+    name = var.rg_name
+    location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+    name = var.virtual_network_name
+    location = var.location
+    resource_group_name = var.rg_name
+
+    address_space = ["10.0.0.0/16"]
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,21 @@ variable "rg_name" {
     description = "Hold resources related to this project"
     type = string
 }
+
+variable "location" {
+    default = "Central US"
+    description = "Resource will be placed in the Central US region"
+    type = string
+}
+
+variable "virtual_network_name" {
+    default = "vnet-deploy"
+    description = "Virtual network"
+    type = string
+}
+
+variable "network_interface_name" {
+    default = "nic-deploy"
+    description = "Resource will be placed in the Central US region"
+    type = string
+}
