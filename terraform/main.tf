0950d4a42fca80af883b940c89fb4b3e18142d05
Luke Mattson <lukemattson03@gmail.com>
2026-08-31 00:02:37 -0500

    Implement subnet and NIC

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,3 +24,33 @@ resource "azurerm_virtual_network" "vnet" {
 
     address_space = ["10.0.0.0/16"]
 }
+
+# Create subnet to hold the VM
+resource "azurerm_subnet" "vm" {
+    name = var.subnet_app_name
+    resource_group_name = var.rg_name
+
+    virtual_network_name = var.virtual_network_name
+    address_prefixes = ["10.0.0.0/24"]
+}
+
+resource "azurerm_public_ip" "public_ip" {
+    name = var.public_ip_name
+    location = var.location
+    resource_group_name = var.rg_name
+
+    sku = "Standard"
+    allocation_method = "Static"
+}
+
+resource "azurerm_network_interface" "nic" {
+    name = var.network_interface_name
+    location = var.location
+    resource_group_name = var.rg_name
+
+    ip_configuration {
+        name = var.nic_ip_config_name
+        public_ip_address_id = azurerm_public_ip.public_ip.id
+        private_ip_address_allocation = "Static"
+    }
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,8 +16,27 @@ variable "virtual_network_name" {
     type = string
 }
 
-variable "network_interface_name" {
-    default = "nic-deploy"
-    description = "Resource will be placed in the Central US region"
+variable "subnet_app_name" {
+    default = "subnet-app"
+    description = "Subnet to hold our app VM"
     type = string
 }
+
+variable "network_interface_name" {
+    default = "nic-app"
+    description = "NIC that attaches to the app VM"
+    type = string
+}
+
+variable "public_ip_name" {
+    default = "public-ip-app"
+    description = "Reach the app from outside Azure"
+    type = string
+}
+
+variable "nic_ip_config_name" {
+    default = "nic-ip-config"
+    description = "Reach the app from outside Azure"
+    type = string
+}
+
